8d153fb974281464db51085d75010cbdc0a7d9d6
Vadym Polianskyi <icvadym.polianskyi@americanwell.com>
2022-01-15 15:36:27 -0600

    Make db_port int

--- app/config/config.py
+++ app/config/config.py
@@ -5,7 +5,7 @@ API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 
 DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
+DB_PORT = int(os.environ.get("DB_PORT"))
 DB_NAME = os.environ.get("DB_NAME")
 DB_USERNAME = os.environ.get("DB_USERNAME")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
